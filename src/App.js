a9c0151f9bc4462c26dac51120b9909df276c2c6
hanna522 <120094054+hanna522@users.noreply.github.com>
2024-04-10 19:18:58 -0700

    add all Route

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './style.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
-// import Menu from "./components/Menu/Menu";
+import Menu from "./components/Menu/Menu";
 import Location from "./components/Location/Location";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer";
+
+
+
 import {
   BrowserRouter as Router,
   Route,
@@ -24,6 +27,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/location" element={<Location />} />
+          <Route path="/menu" element={<Menu />} />
         </Routes>
       <Footer />
       </div>
