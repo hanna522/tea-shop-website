198e5249b4ca6088089ef47d8ee3bd6e6d9c6255
fangyaocneu <156948459+fangyaoccc@users.noreply.github.com>
2024-04-25 01:56:07 -0700

    add menu

--- src/components/Menu/MenuList.js
+++ src/components/Menu/MenuList.js
@@ -8,6 +8,10 @@ import menu7 from '../../Assets/menu/menu7.png';
 import menu8 from '../../Assets/menu/menu8.png';
 import menu9 from '../../Assets/menu/menu9.png';
 import menu10 from '../../Assets/menu/menu10.png';
+import menu11 from '../../Assets/menu/menu11.png';
+import menu12 from '../../Assets/menu/menu12.png';
+import menu13 from '../../Assets/menu/menu13.png';
+import menu14 from '../../Assets/menu/menu14.png';
 
 
 const menus = [
@@ -16,11 +20,15 @@ const menus = [
   { id: 1, category: "featured menu", name: "Green Grape Green Tea", image: menu3, description:"This elegantly flavored green tea fills the mouth with the sweet and savory aroma of the famous Japanese \"Kyoho\" grape. Its refreshing taste makes it ideal for iced teasn 3"  },
   { id: 2, category: "featured menu", name: "Okinawa Brown Sugar Black Tea", image: menu4, description:"Okinawa milk tea is a type of milk tea that draws influence from the Okinawa region of Japan. Often compared to Hokkaido milk tea, Okinawa milk tea is a tasty blend of black tea, milk, and sweetener"  },
   { id: 1, category: "featured menu", name: "Puff Cream Boba Choco Milk Tea", image: menu5, description:"Puff Cream Boba Choco Milk Tea is a decadent beverage combining creamy milk tea with chewy tapioca pearls, topped with a fluffy layer of whipped cream and drizzled with rich chocolate syrup, offering a delightful blend of textures and flavors in every sip."  },
-  { id: 1, category: "featured menu", name: "Oat Milk Matcha Latte", image: menu6, description:"Matcha has a grassy taste, perhaps a nicer way to say that is earthy. It tastes much stronger than regular green tea and has a really unique flavor, so it’s a little hard to describe. If you haven’t tried it, I recommend you just give it a go and see if you like it!" },
-  { id: 2, category: "featured menu", name: "House Black Tea", image: menu7, description:"House Black is a Chinese black tea blend that can be enjoyed hot or iced. It's described as brisk and flavorful with a smooth finish"  },
-  { id: 1, category: "featured menu", name: "Green Grape Green Tea", image: menu8, description:"This elegantly flavored green tea fills the mouth with the sweet and savory aroma of the famous Japanese \"Kyoho\" grape. Its refreshing taste makes it ideal for iced teasn 3"  },
-  { id: 2, category: "featured menu", name: "Okinawa Brown Sugar Black Tea", image: menu9, description:"Okinawa milk tea is a type of milk tea that draws influence from the Okinawa region of Japan. Often compared to Hokkaido milk tea, Okinawa milk tea is a tasty blend of black tea, milk, and sweetener"  },
-  { id: 1, category: "featured menu", name: "Puff Cream Boba Choco Milk Tea", image: menu10, description:"Puff Cream Boba Choco Milk Tea is a decadent beverage combining creamy milk tea with chewy tapioca pearls, topped with a fluffy layer of whipped cream and drizzled with rich chocolate syrup, offering a delightful blend of textures and flavors in every sip."  }
+  { id: 1, category: "featured menu", name: "Lychee Lemon Tea", image: menu6, description:"Lychee lemon tea is a refreshing beverage that combines the sweet, floral notes of lychee with the tart, citrusy zest of lemon. This delightful tea can be served either hot or iced, making it a versatile choice for all seasons. " },
+  { id: 2, category: "featured menu", name: "Peach Green Tea", image: menu7, description:" Type of green tea that is flavored with peach, combining the grassy freshness of green tea with the sweet and aromatic notes of peach."  },
+  { id: 1, category: "featured menu", name: "Marshmallow Milk Tea", image: menu8, description:"Milk tea that is sweetened or topped with marshmallows, adding a creamy and fluffy texture to the beverage."  },
+  { id: 2, category: "featured menu", name: "Orange Green Tea", image: menu9, description:" Type of green tea that is flavored with orange, offering a citrusy twist to the traditional green tea flavor."  },
+  { id: 1, category: "featured menu", name: "Tieguanyin Latte", image: menu10, description:"Latte made with Tieguanyin tea, a premium variety of Chinese oolong tea. The drink combines the floral and slightly sweet notes of Tieguanyin tea with the creamy texture of a latte."  },
+  { id: 1, category: "featured menu", name: "Lemon Black Tea", image: menu11, description:"Type of black tea that is flavored with lemon, offering a refreshing citrus twist to the robust and rich flavor of black tea." },
+  { id: 2, category: "featured menu", name: "Hibiscus Tea", image: menu12, description:"A tea made from the dried petals of the hibiscus flower, known for its vibrant red color and tart, cranberry-like flavor."  },
+  { id: 1, category: "featured menu", name: "Chocolate Latte", image: menu13, description:" A latte that incorporates chocolate, combining the creamy texture of a latte with the rich, sweet flavor of chocolate."  },
+  { id: 1, category: "featured menu", name: "Oreo Milk Tea", image: menu14, description:"A milk tea beverage that includes Oreo cookies, either blended into the tea or added as a topping, creating a unique and popular flavor combination." }
 ];
 
 export default menus;
